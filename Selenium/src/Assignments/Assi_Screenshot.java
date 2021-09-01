9b69c536bb02adc517543ef6ef279f450cf89d21
Sai <Sai@DESKTOP-P55UP23>
2021-09-01 11:17:54 +0530

    First Commit

--- Selenium/src/Assignments/Assi_Screenshot.java
+++ Selenium/src/Assignments/Assi_Screenshot.java
@@ -14,8 +14,6 @@ public class Assi_Screenshot {
 
 	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
-		public static void main(String[] args) {
-			// TODO Auto-generated method stub
 			 System.setProperty("webdriver.chrome.driver","C:\\Users\\Sai\\Documents\\selenium\\chromedriver_win32\\chromedriver.exe");
 				WebDriver driver=new ChromeDriver();
 		        driver.manage().window().maximize();
--- Selenium/src/Sel/Screenshot.java
+++ Selenium/src/Sel/Screenshot.java
@@ -9,21 +9,25 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import com.google.common.io.Files;
 
-public class Assi_Screenshot {
+public class Screenshot {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		 System.setProperty("webdriver.chrome.driver","C:\\Users\\Sai\\Documents\\selenium\\chromedriver_win32\\chromedriver.exe");
 			WebDriver driver=new ChromeDriver();
 	        driver.manage().window().maximize();
-			driver.get("https://www.w3schools.com/");
+			driver.get("https://www.amazon.in/");
 			
 			TakesScreenshot scrshot = ((TakesScreenshot)driver);
 			File scrfile = scrshot.getScreenshotAs(OutputType.FILE);
-			String destfle = "C:\\Users\\Sai\\Pictures\\Screenshots";
+			String destfile = "C:\\Users\\Sai\\Pictures\\Screenshots\\w3school.png";
 			File targetfile = new File(destfile);
-			Files.copy(scrfile.targetfile);
+			Files.copy(scrfile,targetfile);
 			System.out.println("End of Code");
+			driver.close();
+			
+			//File screenshotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+			//FileUtils.copyFile(screenshotFile,new File(".//screenshot//screenshot.png"));
 
 	}
 
